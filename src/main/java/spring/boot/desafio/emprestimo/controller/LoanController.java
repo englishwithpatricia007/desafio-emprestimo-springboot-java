8fd76ff084d29132b02d2a35ee535f72061d9b56
Patricia <englishwithpatricia007@gmail.com>
2025-05-05 14:49:45 -0300

    Versão Pleno - Implementando camadas

--- src/main/java/spring/boot/desafio/emprestimo/controller/LoanController.java
+++ src/main/java/spring/boot/desafio/emprestimo/controller/LoanController.java
@@ -5,54 +5,33 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import spring.boot.desafio.emprestimo.dto.CustomerRequestDTO;
+import spring.boot.desafio.emprestimo.dto.LoanResponseDTO;
+import spring.boot.desafio.emprestimo.services.LoanService;
+
 @RestController
 @RequestMapping("/customer-loans")
 public class LoanController {
+
+    @Autowired
+    private LoanService loanService;
+
+
     @PostMapping
-    public Map<String, Object> getAvailableLoans(@RequestBody Map<String, Object> request) {
-
-        int age = (int) request.get("age");
-        double income = (double) request.get("income");
-        String location = (String) request.get("location");
-        String name = (String) request.get("name");
-
-        List<Map<String, Object>> loans = new ArrayList<>();
-        
-        if(income <= 3000){
-            loans.add(createLoans("PERSONAL", 4));
-            loans.add(createLoans("GUARANTEED", 3));
-
-        }
-
-        if (income > 3000 && income < 5000 && age < 30 && "SP".equalsIgnoreCase(location)) {
-            loans.add(createLoans("PERSONAL", 4));
-            loans.add(createLoans("GUARANTEED", 3));
-        }
-
-        if (income >= 5000) {
-            loans.add(createLoans("CONSIGNMENT", 2));
-
-        }
-
-
+    public Map<String, Object> getAvailableLoans(@RequestBody CustomerRequestDTO customerRequestDTO) {
+        List<LoanResponseDTO> loans = loanService.determineLoans(customerRequestDTO);
 
         Map<String, Object> response = new HashMap<>();
-        response.put("customer", name);
+        response.put("customer", customerRequestDTO.getName());
         response.put("loans", loans);
+
         return response;
-    }
-
-    private Map<String, Object> createLoans(String type, int interestRate) {
-        Map<String, Object> loan = new HashMap<>();
-        loan.put("type", type);
-        loan.put("interest_rate", interestRate);
-
-        return loan;
     }
 
 }
